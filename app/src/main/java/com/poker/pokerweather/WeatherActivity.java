ea9c76d3a78c76376823b568819506346b7c5a06
PokerKun <pocketfan233@gmail.com>
2017-10-11 10:29:40 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/poker/pokerweather/WeatherActivity.java
+++ app/src/main/java/com/poker/pokerweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.poker.pokerweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.poker.pokerweather.gson.Forecast;
 import com.poker.pokerweather.gson.Weather;
+import com.poker.pokerweather.service.AutoUpdateService;
 import com.poker.pokerweather.util.HttpUtil;
 import com.poker.pokerweather.util.Utility;
 
@@ -215,6 +217,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
